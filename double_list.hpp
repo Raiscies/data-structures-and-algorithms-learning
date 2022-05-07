b42ab9b500407ad7ce02bd142a1b58b4d493ec50
Rraiscies <l1258712106@163.com>
2022-05-07 19:43:46 +0800

    added a lot of things for double_list

--- double_list.hpp
+++ double_list.hpp
@@ -5,11 +5,13 @@
 #include <cstddef>
 #include <utility>
 #include <concept>
+#include <initializer_list>
 
 namespace rais::study {
 
 using std::size_t;
 using std::forward;
+using std::initializer_list;
 
 //concepts
 using std::convertible_to;
@@ -24,6 +26,10 @@ struct double_node {
 	template <typename U>
 	requires convertible_to<U, const T&>
 	double_node(U&& val): data(forward<U>(val)) {}
+
+	template <typename U>
+	requires convertible_to<U, const T&>
+	double_node(U&& val, double_node* priv): data(forward<U>(val)), priv{priv} {}
 
 	template <typename U>
 	requires convertible_to<U, const T&>
@@ -94,8 +100,53 @@ public:
 
 
 	double_list() {}
+	double_list(initializer_list<T> list) {
+		const auto it = list.begin();
+		head = new node_t{*it};
+		node_t* pos = head;
+		++it;
+		for(; it != list.end(); ++it) {
+			pos->next = new node_t{i, pos};
+			pos = pos->next;
+		}
+		pos->next = nullptr;
+		head->priv = pos;
+		len = list.size();
+	}
+	double_list(const double_list& other) {
+		const auto it = other.begin();
+		head = new node_t{*it};
+		node_t* pos = head;
+		++it;
+		for(; it != other.end(); ++it) {
+			pos->next = new node_t{i, pos};
+			pos = pos->next;
+		}
+		pos->next = nullptr;
+		head->priv = pos;
+		len = other.len;
+	}
+	double_list(double_list&& other) noexcept: head{other.head}, len{other.len} {
+		other.head = nullptr;
+		other.len = 0;
+	}
+	double_list& operator=(const double_list& other) {
+		if(this == &other) return *this;
+		auto temp = other;
+		return *this = move(temp);	
+	}
+	double_list& operator=(double_list&& other) noexcept{
+		if(this == &other) return *this;
+		clear();
+		head = other.head;
+		len = other.len;
+		other.head = nullptr;
+		other.len = 0;
+		return *this;
+	}
+
 	~double_list() {
-		//TODO
+		clear();
 	}
 
 	size_t length() const noexcept{ return len; } 
@@ -104,8 +155,8 @@ public:
 	//no zero length check
 	T& front() {return head->data; }
 	const T& front() const{return head->data; }
-	T& back() {return tail()->data; }
-	const T& back() const{return tail()->data; }
+	T& back() {return head->priv->data; }
+	const T& back() const{return head->priv->data; }
 
 
 	iterator_t begin()              noexcept{return {head};    }
@@ -152,14 +203,7 @@ public:
 			//insert to the tail
 			head->priv = head->priv->next = new node_t{forward<U>(val), head->priv, nullptr};
 		}else {
-			node_t* pos = head;
-			if(index <= len / 2) {
-				//indexing from head
-				for(size_t i = 0; i < index; i++) pos = pos->next;
-			}else {
-				//indexing from tail
-				for(size_t i = 0; i < (len - index); i++) pos = pos->priv;
-			}
+			node_t* pos = get_node(index);
 			pos->priv = pos->priv->next = new node_t{forward<U>(val), pos->priv, pos};
 		}
 	
@@ -189,15 +233,16 @@ public:
 	}
 	const T& operator[](size_t index) const{
 		//no zero length and boundary check
-		const node_t* pos = head;
-		if(index <= len / 2) {
-			//indexing from head
-			for(size_t i = 0; i < index; i++) pos = pos->next;
-		}else {
-			//indexing from tail
-			for(size_t i = 0; i < (len - index); i++) pos = pos->priv;
-		}
+		const node_t* pos = get_node(index);
 		return pos->data;
+	}
+
+	T* get_ptr(size_t index) {
+		return const_cast<T*>(static_cast<const double_list*>(this)->get_ptr(index));
+	}
+	const T* get_ptr(size_t index) const{
+		if(index >= len) return nullptr;
+		return &get_node(index)->data;
 	}
 
 	T shift() {
@@ -231,27 +276,85 @@ public:
 		return temp;
 	}
 
+	bool erase(size_t index) {
+		if(index >= len) return false;
+		if(index == 0) {
+			//erase head node
+			node_t* old_head = head;
+			head = head->next;
+			if(len != 1) head->priv = old_head->priv;
+			delete old_head;
+		}else if(index == len - 1){
+			//erase tail node
+			node_t* old_tail = head->priv;
+			old_tail->priv->next = nullptr;
+			head->priv = old_tail->priv;
+			delete old_tail;
+		}else {
+			node_t* pos = get_node(index);
+			pos->priv->next = pos->next;
+			pos->next->priv = pos->priv;
+			delete pos;
+		}
+		len--;
+		return true;
+	}
+
+	void clear() {
+		if(head == nullptr) return;
+		node_t* pos = head;
+		for(size_t i = 0; i < len - 1; i++) {
+			pos = pos->next;
+			delete pos->priv;
+		}
+		delete pos;
+		head = nullptr;
+		len = 0;
+	}
+
+	friend void swap(double_list& a, double_list& b) noexcept{
+		node_t* temp_head = a.head;
+		size_t temp_len = a.len;
+		a.head = b.head;
+		a.len = b.len;
+		b.head = temp_head;
+		b.len = temp_len;
+	}
+
+	template <typename OStreamT>
+	requires requires(OutputStreamT& os, const T& val, char c, const char* s) {
+		{os << val}->same_as<OutputStreamT&>;
+		{os << c}->same_as<OutputStreamT&>;
+		{os << s}->same_as<OutputStreamT&>;
+	}
+	friend OStreamT& operator<<(OStreamT& os, const double_list& list) {
+		os << '[';
+		if(list.size() != 0) {
+			os << *list.cbegin();
+			for(auto it = ++list.cbegin(); it != list.cend(); ++it) {
+				os << ", " << *it;
+			}
+		}
+		return os << ']';
+	}
+
 protected:
-	// node_t* tail() noexcept{
-	// 	if(head == nullptr) return nullptr;
-	// 	return head->priv;
-	// }
-	// const node_t* tail() const noexcept{
-	// 	if(head == nullptr) return nullptr;
-	// 	return head->priv;	
-	// } 
-	// node_t*& tail() noexcept{
-	// 	if(head == nullptr) return head;
-	// 	node_t** pos = &head;
-	// 	while((*pos)->next != nullptr) pos = &((*pos)->next);
-	// 	return *pos;
-	// }
-	// const node_t* const & tail() const noexcept{
-	// 	if(head == nullptr) return head;
-	// 	const node_t* const * pos = &head;
-	// 	while((*pos)->next != nullptr) pos = &((*pos)->next);
-	// 	return *pos;
-	// }
+
+	node_t* get_node(size_t index) {
+		return const_cast<node_t*>(static_cast<const double_list*>(this)->get_node(index)); 
+	}
+	const node_t* get_node(size_t index) const{
+		//no boundary and zero length check
+		const node_t* pos = head;
+			if(index <= len / 2) {
+			//indexing from head
+			for(size_t i = 0; i < index; i++) pos = pos->next;
+		}else {
+			//indexing from tail
+			for(size_t i = 0; i < (len - index); i++) pos = pos->priv;
+		}
+		return pos;
+	}
 
 
 }; //class double_list
